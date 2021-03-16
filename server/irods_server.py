4e86741f382d569353d36d655fc083c39b1040cf
Gigabyted <gigabyted224805@gmail.com>
2021-03-16 13:59:39 -0400

    testing stuff

--- server/irods_server.py
+++ server/irods_server.py
@@ -11,7 +11,7 @@ def setup_server():
 	global SERVER_PORT
 	global BUFFER_SIZE
 	global SEPARATOR
-	
+
 	# device's IP address
 	SERVER_HOST = "0.0.0.0"
 	SERVER_PORT = 5001
@@ -25,7 +25,7 @@ def setup_server():
 
 	# bind the socket to our local address
 	s.bind((SERVER_HOST, SERVER_PORT))
-	
+
 def handle_connection():
 	# enabling our server to accept connections
 	# 5 here is the number of unaccepted connections that
@@ -37,9 +37,9 @@ def handle_connection():
 	client_socket, address = s.accept()
 	# if below code is executed, that means the sender is connected
 	print(f"[+] {address} is connected.")
-	
+
 	return (client_socket, address)
-	
+
 def receive_file(args):
 	client_socket = args[0]
 	address = args[1]
@@ -51,7 +51,7 @@ def receive_file(args):
 	filename = os.path.basename(filename)
 	# convert to integer
 	filesize = int(filesize)
-	
+
 	# start receiving the data file from the socket
 	# and writing to the file stream
 	progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
@@ -59,7 +59,7 @@ def receive_file(args):
 		while True:
 			# read 1024 bytes from the socket (receive)
 			bytes_read = client_socket.recv(BUFFER_SIZE)
-			if not bytes_read:    
+			if not bytes_read:
 				# nothing is received
 				# file transmitting is done
 				break
@@ -67,16 +67,19 @@ def receive_file(args):
 			f.write(bytes_read)
 			# update the progress bar
 			progress.update(len(bytes_read))
-			
+
 	unzip_file("./client.zip")
 	put_to_irods(filename, patient_name)
 	return "\nREQ_UPLOAD_FILE by " + address[0] + " fulfilled"
-	
+
 def add_patient(patient_data):
+	# TODO: validation (dir already exists), optional fields functionality
 	data = json.loads(patient_data)
 	dir_path = f"/tempZone/home/public/{data['last_name'].upper()}_{data['first_name'].upper()}"
-	
-	cmdstrs = [f"imkdir {dir_path}"]
+
+	# build a command sequence
+	cmdstrs = []
+	cmdstrs.append(f"imkdir {dir_path}")
 	cmdstrs.append(f"imeta add -C {dir_path} first_name {data['first_name']}")
 	cmdstrs.append(f"imeta add -C {dir_path} last_name {data['last_name']}")
 	cmdstrs.append(f"imeta add -C {dir_path} date_created {data['date_created']}")
@@ -85,18 +88,24 @@ def add_patient(patient_data):
 	cmdstrs.append(f"imeta add -C {dir_path} weight {data['weight']}")
 	cmdstrs.append(f"imeta add -C {dir_path} dob {data['dob']}")
 	cmdstrs.append(f"imeta add -C {dir_path} sex {data['sex']}")
+
+	# optional fields
+	cmdstrs.append(f"imeta add -C {dir_path} middle_name {data['middle_name']}")
 	cmdstrs.append(f"imeta add -C {dir_path} ethnicity {data['ethnicity']}")
-	
+
 	for cmd in cmdstrs:
 		os.system(cmd)
-		
+
 	return"\nREQ_PATIENT_ADD by " + address[0] + " fulfilled"
-		
+
 def edit_patient(patient_data):
 	data = json.loads(patient_data)
 	dir_path = f"/tempZone/home/public/{data['last_name'].upper()}_{data['first_name'].upper()}"
-	
-	cmdstrs = [(f"imeta set -C {dir_path} first_name {data['first_name']}")]
+
+
+	# build a command sequence
+	cmdstrs = []
+	cmdstrs.append(f"imeta set -C {dir_path} first_name {data['first_name']}")
 	cmdstrs.append(f"imeta set -C {dir_path} last_name {data['last_name']}")
 	cmdstrs.append(f"imeta set -C {dir_path} date_created {data['date_created']}")
 	cmdstrs.append(f"imeta set -C {dir_path} date_modified {data['date_modified']}")
@@ -105,28 +114,32 @@ def edit_patient(patient_data):
 	cmdstrs.append(f"imeta set -C {dir_path} dob {data['dob']}")
 	cmdstrs.append(f"imeta set -C {dir_path} sex {data['sex']}")
 	cmdstrs.append(f"imeta set -C {dir_path} ethnicity {data['ethnicity']}")
-	
+
+	# optional fields
+	cmdstrs.append(f"imeta add -C {dir_path} middle_name {data['middle_name']}")
+	cmdstrs.append(f"imeta add -C {dir_path} ethnicity {data['ethnicity']}")
+
 	for cmd in cmdstrs:
 		os.system(cmd)
-		
+
 	return"\nREQ_PATIENT_EDIT by " + address[0] + " fulfilled"
-	
+
 def process_request(client_socket, address):
 	# receive request from client
 	received = client_socket.recv(BUFFER_SIZE).decode()
-	
+
 	print(f"received {received} from {address}")
-	
+
 	request, data = received.split('!')
-	
+
 	print(request)
-	
+
 	switcher = {
 		"REQ_UPLOAD_FILE": receive_file,
 		"REQ_PATIENT_ADD": add_patient,
 		"REQ_PATIENT_EDIT": edit_patient
 		}
-	
+
 	args = {
 		"REQ_UPLOAD_FILE": [client_socket, address, data],
 		"REQ_PATIENT_ADD": (data),
@@ -134,14 +147,23 @@ def process_request(client_socket, address):
 		}
 	message = switcher[request](args[request])
 	print(message)
-	
+
+def fetch_patient_data():
+	cmdstrs = []
+	cmdoutputs = []
+
+	cmdstrs.append("ils /tempZone/home/public | awk -F '/' '{print $5}'")
+
+	for cmd in cmdstrs:
+		cmdoutputs.append((subprocess.check_output(cmd)).split("\n"))
+
 def download_meta_default(addr, patient_name):
     # supplies metadata on the most recently accessed or uploaded patient files
 	# if date_create or date_modify are within the last (x amount of time)
 	cmdstr = "imeta qu -d date_create like 12/__/2020 | awk '/dataObj:/ {print $2}'"
 	# imeta qu -d date_create_month 'n<=' 12 | awk '/dataObj:/ {print $2}'
 	print(cmdstr)
-	
+
 def unzip_file(path):
 	with zipfile.ZipFile(path, 'r') as zip_ref:
 		zip_ref.extractall("./temp")
@@ -150,21 +172,21 @@ def unzip_file(path):
 def put_to_irods(filename, patient_name):
 	with open("./temp/meta.txt") as f:
 		data = json.load(f)
-		
+
 	# build the string of the command that will iput the file with metadata attached
 	cmdstr = f"iput ./temp/{filename} /tempZone/home/public/{patient_name}/{filename} --metadata=\"date_create;{data['date']};;title;{data['title']};;overseeing;{data['overseeing']};;notes;{data['notes']};;\""
 	os.system("echo " + cmdstr)
 	os.system(cmdstr)
-	
-setup_server()
-#while True:
-client_socket, address = handle_connection()
-process_request(client_socket, address)
+
+# setup_server()
+# #while True:
+# client_socket, address = handle_connection()
+# process_request(client_socket, address)
 
 # close the server socket
-s.close()
-	
+# s.close()
 
+fetch_patient_data()
 
 #def update_client_meta:
     # for each loop that goes through the client whitelist and sends each IP
@@ -179,4 +201,4 @@ s.close()
 #def download_file(addr, patient, path):
     # send the full specified file to the client
 	#file, meta = s.recvfrom(256)
-	#command = iput file 
+	#command = iput file
