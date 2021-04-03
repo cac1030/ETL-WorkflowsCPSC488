7403e652e45576f66270ecd2803995110baf65a5
Gigabyted <gigabyted224805@gmail.com>
2021-04-03 00:29:52 -0400

    add_patient now checks if patient exists

--- client/irods_patient.py
+++ client/irods_patient.py
@@ -2,21 +2,22 @@ import socket
 import tqdm
 import sys
 import transaction
+import argparse
+import os
 
 parser = argparse.ArgumentParser()
 group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument("-a", "--add", action="store_true", help="add new patient")
-group.add_argument("-e", "--edit", action="store_true", help="edit existing patient")
+group.add_argument("-a", "--add", help="add new patient")
+group.add_argument("-e", "--edit", help="edit existing patient")
 group.add_argument("-f", "--fetch", action="store_true", help="fetch data on all patients")
-parser.add_argument("json", help="the json string containing new patient data")
 args = parser.parse_args()
 
 trans = transaction.Request()
 trans.connect()
 if args.add:
-    trans.send_req("REQ_PATIENT_ADD", sys.argv[1].upper())
+    trans.send_req("REQ_PATIENT_ADD", args.add)
 elif args.edit:
-    trans.send_req("REQ_PATIENT_EDIT", sys.argv[1].upper())
+    trans.send_req("REQ_PATIENT_EDIT", args.edit)
 elif args.fetch:
     trans.send_req("REQ_PATIENT_FETCH", "null")
 
