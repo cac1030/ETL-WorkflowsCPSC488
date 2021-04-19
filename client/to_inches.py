368296a7440bb89c3a444b431e029a7b7218046f
Gigabyted <gigabyted224805@gmail.com>
2021-04-19 19:19:45 -0400

    debug

--- client/to_inches.py
+++ client/to_inches.py
@@ -2,6 +2,7 @@ import subprocess
 
 cmd = "ils | cut -f4 -d ' '"
 all_patients = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
+print(all_patients)
 
 for patient in all_patients:
     cmd = f"imeta set -C {patient} height 68"
